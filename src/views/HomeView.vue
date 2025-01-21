9c6fda59f0499fa8ab9ea3571fc63fb5c208f070
Guy Nguyen-Phuoc <guy.nguyen.phuoc@gmail.com>
2025-01-20 17:40:52 -1000

    added views, routers, home/about pages, picocss, and a form

---
+++ src/views/HomeView.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="form-container">
+    <form>
+    <fieldset>
+      <label>
+        First Name
+        <input
+          name="first_name"
+          placeholder="First name"
+          autocomplete="given-name"
+        />
+      </label>
+      <label>
+        Email
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          autocomplete="email"
+        />
+      </label>
+    </fieldset>
+
+    <input
+      type="submit"
+      value="Subscribe"
+    />
+  </form>
+  </div>
+</template>
+
+<style>
+  .form-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    min-height: 80vh;
+  }
+
+  form {
+    width: 40%;
+  }
+</style>
