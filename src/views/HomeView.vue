bb7043dfb0a703ba0b131a30725e775f90f7ee0b
Guy Nguyen-Phuoc <guy.nguyen.phuoc@gmail.com>
2025-01-22 19:05:35 -1000

    added welcome text

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,9 +2,18 @@
 </script>
 
 <template>
-
+  <h1>Welcome to the <br><span class="blue-text">Honolulu Go Camp!</span></h1>
 </template>
 
-<style>
+<style scoped>
+  h1 {
+    padding-top: 10vh;
+    text-align: center;
+    color: rgb(6, 30, 47);
+    font-size: 70px;
+  }
 
+  .blue-text {
+    color: rgb(1, 127, 192);
+  }
 </style>
