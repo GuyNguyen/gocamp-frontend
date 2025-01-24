a410b6e1ed508da5888fce8d2cf634b7b44974d7
Guy Nguyen-Phuoc <guy.nguyen.phuoc@gmail.com>
2025-01-24 01:09:39 -1000

    initial commit

---
+++ src/components/Footer.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="footer">
+    <div class="footer-left-content">
+      <h2>Contact</h2>
+      <h5>Phone</h5>
+      <a class="link-footer" href="tel:(808)555-0120">(808)555-0120</a> <br>
+      <a class="link-footer" href="tel:(808)555-0116">(808)555-0116</a>
+    </div>
+    <div class="footer-right-content"></div>
+  </div>
+</template>
+
+<style scoped>
+  .footer {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    bottom: 0;
+    justify-content: space-between;
+    width: 100%;
+    position: fixed;
+  }
+</style>
