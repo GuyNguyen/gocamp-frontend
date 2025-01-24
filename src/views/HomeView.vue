14db8d24a422f5b24acff87602f5efa34f7bf6d8
Guy Nguyen-Phuoc <guy.nguyen.phuoc@gmail.com>
2025-01-24 01:14:06 -1000

    Added footer and welcome header

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,15 +2,37 @@
 </script>
 
 <template>
-  <h1>Welcome to the <br><span class="blue-text">Honolulu Go Camp!</span></h1>
+  <div class="home">
+    <h1>Welcome to the <br><span class="blue-text">Honolulu Go Camp!</span></h1>
+    <div class="about-button">
+      <RouterLink to="/about"><button class="outline">About Camp</button></RouterLink>
+    </div>
+  </div>
+
+
 </template>
 
 <style scoped>
+  .home {
+    display: flex;
+    position: relative;
+    flex-direction: column;
+    justify-content: center;
+
+  }
+
   h1 {
     padding-top: 10vh;
     text-align: center;
     color: rgb(6, 30, 47);
-    font-size: 70px;
+    font-size: 100px;
+    flex-wrap: wrap;
+  }
+
+  .about-button {
+    display: flex;
+    padding-top: 50px;
+    justify-content: center;
   }
 
   .blue-text {
