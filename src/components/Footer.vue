df073ec2c15e41032cc13a6d16d6e03c0afc8b05
Guy Nguyen-Phuoc <guy.nguyen.phuoc@gmail.com>
2025-01-26 12:36:13 -1000

    added div to control flex

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,13 +3,18 @@
 
 <template>
   <div class="footer">
-    <div class="footer-left-content">
-      <h2>Contact</h2>
-      <h5>Phone</h5>
-      <a class="link-footer" href="tel:(808)555-0120">(808)555-0120</a> <br>
-      <a class="link-footer" href="tel:(808)555-0116">(808)555-0116</a>
+    <div class="footer-content">
+      <div class="footer-left-content">
+        <h2>Contact</h2>
+        <h5>Phone</h5>
+        <a class="link-footer" href="tel:(808)555-0120">(808)555-0120</a> <br>
+        <a class="link-footer" href="tel:(808)555-0116">(808)555-0116</a>
+      </div>
+
+      <div class="footer-right-content">
+        <h2>LET'S GET IN TOUCH</h2>
+      </div>
     </div>
-    <div class="footer-right-content"></div>
   </div>
 </template>
 
@@ -22,5 +27,27 @@
     width: 100%;
     padding-top: 50px;
     position: relative;
+  }
+
+  .footer-content {
+    display: flex;
+    width: 100%;
+  }
+
+  .footer-left-content {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: space-between;
+    width: 50%;
+    padding-top: 50px;
+  }
+
+  .footer-right-content {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    width: 60%;
+    padding-top: 50px;
   }
 </style>
