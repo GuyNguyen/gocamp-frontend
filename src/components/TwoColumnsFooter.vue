6c163286c73f3f8d22f2f9ca759571bd84d0dca3
Guy Nguyen-Phuoc <guy.nguyen.phuoc@gmail.com>
2025-01-27 11:01:16 -1000

    added two columns input labels for footer form

---
+++ src/components/TwoColumnsFooter.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="grid">
+    <label>
+      Name
+      <input
+        name="name"
+        placeholder="Your name"
+      />
+    </label>
+    <label>
+      Phone
+      <input
+        name="phone"
+        placeholder="Your phone"
+        autocomplete="tel-national"
+      />
+    </label>
+  </div>
+</template>
+
+<style scoped>
+  .grid {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    grid-template-rows: auto;
+  }
+</style>
